f500bb70c9417ae810ad97afb07bc6fcd6cfd040
Rfernandez3-Cide <rfernandez3@nigul.cide.es>
2025-10-20 18:47:28 +0200

    Version with out comments

--- u1ex02b/src/main/java/u1ex02b/Principal.java
+++ u1ex02b/src/main/java/u1ex02b/Principal.java
@@ -1,6 +1,8 @@
 package u1ex02b;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
@@ -12,10 +14,15 @@ import org.xml.sax.helpers.DefaultHandler;
 
 class LibroHandler extends DefaultHandler {
     private StringBuilder value = new StringBuilder();
+    private String añoActual;
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         value.setLength(0);
+
+        if (qName.equals("libro")) {
+            añoActual = attributes.getValue("año");
+        }
     }
 
     @Override
@@ -25,24 +32,30 @@ class LibroHandler extends DefaultHandler {
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
+        String texto = value.toString();
         switch (qName) {
             case "libro":
                 System.out.println("");
                 break;
-            case "título":
-                System.out.println("Título del libro: " + this.value.toString());
+            case "titulo":
+                System.out.println("Título del libro: " + texto);
+                Principal.escribirTxt(añoActual, "Título del libro: " + texto);
                 break;
             case "apellido":
-                System.out.println("Apellido del autor: " + this.value.toString());
+                System.out.println("Apellido del autor: " + texto);
+                Principal.escribirTxt(añoActual, "Apellido del autor: " + texto);
                 break;
             case "nombre":
-                System.out.println("Nombre del autor: " + this.value.toString());
+                System.out.println("Nombre del autor: " + texto);
+                Principal.escribirTxt(añoActual, "Nombre del autor: " + texto);
                 break;
-            case "editiorial":
-                System.out.println("Editiorial del libro: " + this.value.toString());
+            case "editorial":
+                System.out.println("Editorial del libro: " + texto);
+                Principal.escribirTxt(añoActual, "Editorial del libro: " + texto);
                 break;
             case "precio":
-                System.out.println("Precio del libro: " + this.value.toString());
+                System.out.println("Precio del libro: " + texto);
+                Principal.escribirTxt(añoActual, "Precio del libro: " + texto);
                 break;
         }
     }
@@ -59,4 +72,16 @@ public class Principal {
             System.out.println("Error: " + ex.getMessage());
         }
     }
+
+    public static void escribirTxt(String año, String contenido) {
+        String nombreFichero = "libros_" + año + ".txt";
+
+        try (PrintWriter out = new PrintWriter(new FileWriter(nombreFichero, true))) {
+            out.println(contenido);
+        } catch (IOException e) {
+            System.out.println("Error escribiendo el fichero para el año " + año);
+            e.printStackTrace();
+        }
+    }
+
 }
